996598c2eaba33100655d65e0adb1565062d3385
ajiva84 <jivazam@gmail.com>
2021-01-13 01:27:52 +0000

    first commit

---
+++ src/js/views/dashboard.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const Dashboard = props => {};
+
+Dashboard.propTypes = {
+	match: PropTypes.object
+};
