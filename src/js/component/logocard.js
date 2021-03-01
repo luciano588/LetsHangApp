1acf0b5024bd246f31471e0b0954d433610e5b61
ajiva84 <jivazam@gmail.com>
2021-03-01 18:35:48 +0000

    update-logo

--- src/js/component/logocard.js
+++ src/js/component/logocard.js
@@ -4,14 +4,14 @@ import { useSpring, animated } from "react-spring";
 import PropTypes from "prop-types";
 import logo from "/workspace/LetsHangApp/src/img/logo.png";
 
-const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
-const trans1 = (x, y) => `translate3d(${x / 8}px,${y / 3}px,0)`;
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 8];
+const trans1 = (x, y) => `translate3d(${x / 8}px,${y / 2 + 4}px,0)`;
 const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`;
 const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`;
 const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`;
 
 export const Card = () => {
-	const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 40 } }));
+	const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 5, tension: 200, friction: 20 } }));
 	return (
 		<div className="container" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
 			<animated.div style={{ transform: props.xy.interpolate(trans1) }}>
